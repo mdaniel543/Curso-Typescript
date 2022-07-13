8becd2a8300025cb7b35475268dfae6c186de847
Daniel Rodriguez <rodriguezmarvindaniel@gmail.com>
2022-07-12 21:17:37 -0600

    Se inicio angular

---
+++ client/src/app/models/Game.ts
@@ -0,0 +1,9 @@
+import { Title } from '@angular/platform-browser';
+
+export interface Game {
+    id_game?: number,
+    title?: string,
+    description?: string,
+    image?: string,
+    created_at?: Date
+};
