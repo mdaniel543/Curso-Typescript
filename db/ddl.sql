0da873d191770ee3ffd59d3bea5ac9e479e5046c
Marvin Daniel Rodriguez <rodriguezmarvindaniel@gmail.com>
2022-07-19 11:27:38 -0600

    se crea una anotacion

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,3 +1,8 @@
+--sudo mysql -u root -p
+--https://www.inmotionhosting.com/support/server/databases/create-a-mysql-database/
+
+CREATE DATABASE prueba;
+
 Create table game(
     id_game int auto_increment primary key,
     title varchar(100),
